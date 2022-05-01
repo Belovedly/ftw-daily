cd1ecd5a0368bd948c9a48d8132941f189709842
Amado Tanglao <tedserdel@gmail.com>
2022-05-01 13:18:07 -0700

    Activated sort search by distance

--- server/sitemap.js
+++ server/sitemap.js
@@ -61,7 +61,7 @@ exports.sitemapStructure = () => {
       '/': ['get'],
       '/signup': ['get'],
       '/login': ['get'],
-      '/s?address=Helsinki%2C%20Finland%26bounds=60.2978389%2C25.254484899999966%2C59.9224887%2C24.782875800000056%26country=FI%26origin=60.16985569999999%2C24.93837910000002': [
+      '/s?address=Seattle%2C%20Washington%2C%20United%20States&bounds=47.734163%2C-122.218866%2C47.479248%2C-122.435899': [
         'get',
       ],
       '/account': ['get'],
@@ -84,7 +84,7 @@ exports.sitemapStructure = () => {
         changefreq: 'always',
         priority: 1.0,
       },
-      '/s?address=Helsinki%2C%20Finland%26bounds=60.2978389%2C25.254484899999966%2C59.9224887%2C24.782875800000056%26country=FI%26origin=60.16985569999999%2C24.93837910000002': {
+      '/s?address=Seattle%2C%20Washington%2C%20United%20States&bounds=47.734163%2C-122.218866%2C47.479248%2C-122.435899': {
         changefreq: 'always',
         priority: 1.0,
       },
