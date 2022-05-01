392785c12b57b16fb164fc1dadf40ee4f6d068d2
Amado Tanglao <tedserdel@gmail.com>
2022-05-01 12:58:45 -0700

    Updated social links and map fuzzy

--- src/config.js
+++ src/config.js
@@ -96,13 +96,13 @@ const canonicalRootURL = process.env.REACT_APP_CANONICAL_ROOT_URL;
 const siteTitle = 'Beloved';
 
 // Twitter handle is needed in meta tags (twitter:site). Start it with '@' character
-const siteTwitterHandle = '@beloved';
+const siteTwitterHandle = '@joinbeloved';
 
 // Instagram page is used in SEO schema (http://schema.org/Organization)
-const siteInstagramPage = null;
+const siteInstagramPage = 'https://www.instagram.com/joinbeloved/';
 
 // Facebook page is used in SEO schema (http://schema.org/Organization)
-const siteFacebookPage = 'https://www.facebook.com/Beloved/';
+const siteFacebookPage = null;
 
 // Social logins & SSO
 
@@ -139,7 +139,7 @@ const maps = {
     // Limit location autocomplete to a one or more countries
     // using ISO 3166 alpha 2 country codes separated by commas.
     // If you want to limit the autocomplete, uncomment this value:
-    // countryLimit: ['AU'],
+      countryLimit: ['US'],
   },
 
   // When fuzzy locations are enabled, coordinates on maps are
@@ -149,7 +149,7 @@ const maps = {
   // coordinates are still accessible in the HTTP requests and the
   // Redux store.
   fuzzy: {
-    enabled: false,
+    enabled: true,
 
     // Amount of maximum offset in meters that is applied to obfuscate
     // the original coordinates. The actual value is random, but the
@@ -162,14 +162,14 @@ const maps = {
     defaultZoomLevel: 13,
 
     // Color of the circle on the Map component.
-    circleColor: '#c0392b',
+    circleColor: '#ff6852',
   },
 
   // Custom marker image to use in the Map component.
   //
   // NOTE: Not used if fuzzy locations are enabled.
   customMarker: {
-    enabled: false,
+    enabled: true,
 
     // Publicly accessible URL for the custom marker image.
     //
