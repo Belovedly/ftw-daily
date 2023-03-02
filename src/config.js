a322d8180c3003ec4eb5067eb4f0825bdb2e57cd
Amado Tanglao <tedserdel@gmail.com>
2023-03-01 20:39:13 -0800

    Marker offset does not change the marker radius

--- src/config.js
+++ src/config.js
@@ -156,9 +156,9 @@ const maps = {
 
     // Amount of maximum offset in meters that is applied to obfuscate
     // the original coordinates. The actual value is random, but the
-    // obfuscated coordinates are withing a circle that has the same
+    // obfuscated coordinates are within a circle that has the same
     // radius as the offset.
-    offset: 4000,
+    offset: 500,
 
     // Default zoom level when showing a single circle on a Map. Should
     // be small enough so the whole circle fits in.
